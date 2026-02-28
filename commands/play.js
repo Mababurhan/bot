4215a302098e8bca60d5d51cd21f4b37085ca810
Mababurhan <74769699+Mababurhan@users.noreply.github.com>
2026-02-28 03:32:18 +0300

    Refactor play command to use context and args

--- commands/play.js
+++ commands/play.js
@@ -1,17 +1,12 @@
-
 const { 
-  SlashCommandBuilder, 
-  EmbedBuilder, 
-  ActionRowBuilder, 
-  ButtonBuilder, 
-  ButtonStyle 
+  SlashCommandBuilder,
+  EmbedBuilder 
 } = require('discord.js');
 
 const { 
-  joinVoiceChannel, 
-  createAudioPlayer, 
-  createAudioResource, 
-  AudioPlayerStatus 
+  joinVoiceChannel,
+  createAudioPlayer,
+  createAudioResource
 } = require('@discordjs/voice');
 
 const play = require('play-dl');
@@ -19,97 +14,59 @@ const play = require('play-dl');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Play music from YouTube')
+    .setDescription('Play music')
     .addStringOption(option =>
       option.setName('url')
         .setDescription('YouTube URL')
         .setRequired(true)
     ),
 
-  async execute(interaction) {
-    const url = ctx.options 
-  ? ctx.options.getString('url') 
-  : args[0];
-    const voiceChannel = interaction.member.voice.channel;
+  async execute(ctx, args) {
 
-    if (!voiceChannel)
-      return ctx.reply(...)({ content: '🎤 پێویستە بچیتە ژووری دەنگی', ephemeral: true });
+    // 🎯 اگر Slash بوو
+    const url = ctx.options
+      ? ctx.options.getString('url')
+      : args[0];
 
-    const info = await play.video_info(url);
-    const title = info.video_details.title;
-    const thumbnail = info.video_details.thumbnails[0].url;
+    if (!url)
+      return ctx.reply
+        ? ctx.reply('🎶 لینک بنێرە')
+        : ctx.channel.send('🎶 لینک بنێرە');
+
+    const voiceChannel = ctx.member.voice.channel;
+
+    if (!voiceChannel) {
+      return ctx.reply
+        ? ctx.reply({ content: '🎤 پێویستە بچیتە ژووری دەنگی', ephemeral: true })
+        : ctx.channel.send('🎤 پێویستە بچیتە ژووری دەنگی');
+    }
 
     const connection = joinVoiceChannel({
       channelId: voiceChannel.id,
-      guildId: interaction.guild.id,
-      adapterCreator: interaction.guild.voiceAdapterCreator
+      guildId: ctx.guild.id,
+      adapterCreator: ctx.guild.voiceAdapterCreator
     });
 
     const stream = await play.stream(url);
+
     const resource = createAudioResource(stream.stream, {
       inputType: stream.type
     });
 
     const player = createAudioPlayer();
+
     connection.subscribe(player);
     player.play(resource);
 
-    // 🎨 Embed
     const embed = new EmbedBuilder()
       .setColor('#2b2d31')
-      .setTitle('🎶 Now Playing')
-      .setDescription(`**${title}**`)
-      .setThumbnail(thumbnail)
-      .setFooter({ text: `Requested by ${interaction.user.username}` })
-      .setTimestamp();
+      .setTitle('🎶 Playing Music')
+      .setDescription(url);
 
-    // 🎛️ Buttons
-    const row = new ActionRowBuilder().addComponents(
-      new ButtonBuilder()
-        .setCustomId('pause')
-        .setLabel('Pause')
-        .setStyle(ButtonStyle.Secondary),
-
-      new ButtonBuilder()
-        .setCustomId('resume')
-        .setLabel('Resume')
-        .setStyle(ButtonStyle.Success),
-
-      new ButtonBuilder()
-        .setCustomId('stop')
-        .setLabel('Stop')
-        .setStyle(ButtonStyle.Danger)
-    );
-
-    await interaction.reply({ embeds: [embed], components: [row] });
-
-    const collector = interaction.channel.createMessageComponentCollector({
-      time: 600000
-    });
-
-    collector.on('collect', async i => {
-      if (i.user.id !== interaction.user.id)
-        return i.reply({ content: '❌ تۆ نیت', ephemeral: true });
-
-      if (i.customId === 'pause') {
-        player.pause();
-        await i.reply({ content: '⏸️ Paused', ephemeral: true });
-      }
-
-      if (i.customId === 'resume') {
-        player.unpause();
-        await i.reply({ content: '▶️ Resumed', ephemeral: true });
-      }
-
-      if (i.customId === 'stop') {
-        player.stop();
-        connection.destroy();
-        await i.reply({ content: '⏹️ Stopped & Left', ephemeral: true });
-      }
-    });
-
-    player.on(AudioPlayerStatus.Idle, () => {
-      connection.destroy();
-    });
+    if (ctx.reply) {
+      await ctx.reply({ embeds: [embed] });
+    } else {
+      await ctx.channel.send({ embeds: [embed] });
+    }
   }
 };
